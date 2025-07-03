ed22697d4df0823ced61bad566beb331ef20ec11
aksoy <bbcc8417@naver.com>
2025-07-03 09:31:29 +0900

    test

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React 앱 주소
+    allow_origins=["*"],  # 또는 ['http://localhost:3000'] 등 명확히
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
